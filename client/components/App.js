48d3b82ef7e3b26388140869f0c9f7f3e3abed2e
Rafal Witczak <rwitczak89@gmail.com>
2021-01-29 18:03:51 +0100

    feat(header): add header to app

--- client/components/App.js
+++ client/components/App.js
@@ -1,8 +1,15 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from './header/Header';
+
 const App = () => {
-  return <div className="hello">Hello World from App.js!</div>;
+  return (
+    <div className="hello">
+      <Header />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ client/components/header/Header.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__text">URL Shortster service</div>
+    </div>
+  );
+};
+
+export default Header;
