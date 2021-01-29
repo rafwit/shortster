0f22b78f1b555a566dac94810de058baa3d44a3d
Rafal Witczak <rwitczak89@gmail.com>
2021-01-29 14:34:10 +0100

    refactor(router): update path to import handleError function

--- src/router.js
+++ src/router.js
@@ -2,15 +2,20 @@ const router = require('express').Router();
 const {
   saveUrl,
   retrieveSavedUrlIfAvailable,
+  serveStatsForShortUrl,
 } = require('./controllers/shortser.controller');
-const { handleError } = require('./middleware/handle.errors');
+const { handleError } = require('./helpers/handle.errors');
 
 router.post('/', saveUrl);
 
 router.post('*', handleError);
 
 router.get('/:shortUrl', retrieveSavedUrlIfAvailable);
+router.get('/:shortUrl/stats', serveStatsForShortUrl);
 
 router.get('*', handleError);
+router.get('*', (req, res) => {
+  res.status(404).send("We're sorry, the requested page was not found");
+});
 
 module.exports = router;
