299620754b7f76bf1d825336cd59ffdf508100b9
Rafal Witczak <rwitczak89@gmail.com>
2021-01-29 13:54:17 +0100

    feat(router): update router to serve path '/:shortUrl'

--- src/router.js
+++ src/router.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
-const { saveUrl } = require('./controllers/shortser.controller');
+const {
+  saveUrl,
+  retrieveSavedUrlIfAvailable,
+} = require('./controllers/shortser.controller');
 const { handleError } = require('./middleware/handle.errors');
 
 router.post('/', saveUrl);
 
 router.post('*', handleError);
 
-router.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+router.get('/:shortUrl', retrieveSavedUrlIfAvailable);
 
 router.get('*', handleError);
 
