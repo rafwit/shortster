ee5e4d5ae7d53fd631e1ca3eb1493a6502aa888a
Rafal Witczak <rwitczak89@gmail.com>
2021-01-29 13:30:03 +0100

    feat(router): update router to serve path '/' and add error handling middleware

--- src/router.js
+++ src/router.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const { saveUrl } = require('./controllers/shortser.controller');
+const { handleError } = require('./middleware/handle.errors');
+
+router.post('/', saveUrl);
+
+router.post('*', handleError);
 
 router.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+router.get('*', handleError);
 
 module.exports = router;
